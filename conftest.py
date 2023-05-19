2e8b966d1cc0a56dad2c08c4d6868e99aaefc31f
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-05-19 13:51:30 +0300

    Changed aliases for browsers options

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as chrome_options
-from selenium.webdriver.firefox.options import Options as firefox_options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
@@ -17,14 +18,13 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_lang = request.config.getoption("language")
 
-
     if browser_name == "chrome":
-        options = chrome_options()
+        options = ChromeOptions()
         print("\nstart chrome browser for test..")
         options.add_experimental_option('prefs', {'intl.accept_languages': user_lang})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        options = firefox_options()
+        options = FirefoxOptions()
         options.set_preference("intl.accept_languages", user_lang)
         browser = webdriver.Firefox(options=options)
         print("\nstart firefox browser for test..")
